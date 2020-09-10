a030c63fd328a55e7b91116f8d26c3865a24af68
dench1k <denmoskvitin@gmail.com>
2020-09-10 14:23:18 +0300

    feat: add svgsprite, webp manual tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,12 @@ const concat = require('gulp-concat');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const flexbugs = require('postcss-flexbugs-fixes');
-const minmax = require('postcss-media-minmax');
 const cssnano = require('cssnano');
 const rename = require("gulp-rename");
 const babel = require('gulp-babel');
 const terser = require('gulp-terser');
 const imagemin = require('gulp-imagemin');
-const webp = require('gulp-webp');
+const webpPlugin = require('gulp-webp');
 const svgstore = require('gulp-svgstore');
 const del = require('del');
 const server = require('browser-sync').create();
@@ -21,9 +20,7 @@ const server = require('browser-sync').create();
  */
 // const gulpif = require('gulp-if');
 
-const processors = [minmax(),
-  autoprefixer(),
-  flexbugs(), cssnano];
+const processors = [autoprefixer(), flexbugs(), cssnano()];
 
 function setMode(isProduction = false) {
   return cb => {
@@ -72,7 +69,7 @@ function images() {
         quality: 75,
         progressive: true
       }),
-      imagemin.optipng({optimizationLevel: 5}),
+      imagemin.optipng({optimizationLevel: 3}),
       imagemin.svgo({
         plugins: [
           {removeViewBox: true},
@@ -85,9 +82,25 @@ function images() {
     .pipe(dest('build/images'));
 }
 
+// TODO: make automated
+function sprite() {
+  return src("src/images/icons/{icon-*,logo-*}.svg")
+    .pipe(svgstore({inlineSvg: true}))
+    .pipe(rename("sprite_auto.svg"))
+    .pipe(dest("build/images/icons"));
+}
+
+// TODO: make automated
+function webp() {
+  return src("build/images/**/*.{png,jpg}")
+    .pipe(webpPlugin({quality: 85}))
+    .pipe(dest("build/images"));
+}
+
 function clean() {
   return del('build');
 }
+
 
 function refresh(cb) {
   server.reload();
@@ -118,6 +131,8 @@ exports.fonts = fonts;
 exports.styles = styles;
 exports.scripts = scripts;
 exports.images = images;
+exports.sprite = sprite;
+exports.webp = webp;
 exports.clean = clean;
 exports.serve = serve;
 
