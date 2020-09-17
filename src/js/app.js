464f369794cc65a2b2c2de80421a7223c4e0c5c0
dench1k <denmoskvitin@gmail.com>
2020-09-17 10:39:37 +0300

    test: try git hooks with husky

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,7 @@ const writeMsg = (msg) => msg; console.log(writeMsg("test"));
 
 class Egz {
   constructor() { this.make = "egz";}
-  render() {
-    console.log(this.make);
-  }
+  render() { console.log(this.make); }
 }
 
 const example = new Egz();
